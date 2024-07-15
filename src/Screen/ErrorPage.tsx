aca5fbcc5460ba13584c9b06d8569f2b3aea737c
tommoon <tomasbtmoon@gmail.com>
2024-07-15 13:42:49 +0200

    fix

--- src/Screen/ErrorPage.tsx
+++ src/Screen/ErrorPage.tsx
@@ -1,17 +1,28 @@
 // src/components/ErrorPage.tsx
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 const ErrorPage: React.FC = () => {
   const error = useRouteError();
   console.error(error);
+
+  // Type guard for narrowing the error type
+  const errorMessage = (() => {
+    if (isRouteErrorResponse(error)) {
+      return error.statusText;
+    }
+    if (error instanceof Error) {
+      return error.message;
+    }
+    return "An unknown error occurred";
+  })();
 
   return (
     <div>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{errorMessage}</i>
       </p>
     </div>
   );
