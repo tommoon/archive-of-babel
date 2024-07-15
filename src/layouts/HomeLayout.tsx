3a73f6a8c9029cf5d8932d84916be3148641a986
tommoon <tomasbtmoon@gmail.com>
2024-07-15 15:15:48 +0200

    updating home page

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -2,9 +2,12 @@ import { Footer } from "@/Screen/Home/components/Footer";
 import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { Spin as Hamburger } from 'hamburger-react';
+import { BookText, GraduationCap, TextSearch } from "lucide-react";
 
 export const HomeLayout = () => {
   const [yPos, setYPos] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => setYPos(window.scrollY);
@@ -14,34 +17,48 @@ export const HomeLayout = () => {
     };
   }, []);
 
+
+
   return (
     <div className="relative text-base-content">
       <div
         className={cn(
           "navbar fixed top-0 left-0 right-0 z-30 transition-colors",
           yPos > 0 && "bg-base-200",
-          "justify-center",
+          isOpen && "bg-base-200",
+          "justify-center"
         )}
       >
         <a className=" sm:text-2xl font-bold mx-auto">Archive of Babel</a>
         <div className="flex-none absolute right-0 mr-4">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
+            <Hamburger toggled={isOpen} toggle={setIsOpen} />
         </div>
+        {isOpen && (
+          <div className="menu absolute top-14 right-0 z-20 w-full sm:w-auto bg-base-200">
+            <ul className="menu rounded-box w-full sm:w-56 bg-base-200">
+            <li>
+              <a>
+                <TextSearch/>
+                Advanced Search
+              </a>
+            </li>
+            <li>
+              <a>
+                <BookText/>
+                Theory
+              </a>
+            </li>
+            <li>
+              <a>
+                <GraduationCap/>
+                Tutorials
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
       </div>
+
       <Outlet />
       <Footer />
     </div>
