1d5534c45227c29508c925cfd501c11f21d39e8b
tommoon <tomasbtmoon@gmail.com>
2024-08-07 15:59:07 +0200

    update analytics trackign

--- src/useAnalyticsTracker.ts
+++ src/useAnalyticsTracker.ts
@@ -2,13 +2,18 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga4";
 
+// Determine if the application is running in development mode
+const isDevelopment = window.location.hostname === "localhost";
+
 export const useAnalyticsTracker = () => {
   const location = useLocation();
 
   useEffect(() => {
-    ReactGA.send({
-      hitType: "pageview",
-      page: location.pathname + location.search,
-    });
+    if (!isDevelopment && location.hash) {
+      ReactGA.send({
+        hitType: "pageview",
+        page: location.pathname + location.search,
+      });
+    }
   }, [location]);
 };
