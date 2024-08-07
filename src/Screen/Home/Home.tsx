bb825ef42b59c0900402c62e89b6c945edd16b8b
tommoon <tomasbtmoon@gmail.com>
2024-08-07 10:13:55 +0200

    update meta

--- src/Screen/Home/Home.tsx
+++ src/Screen/Home/Home.tsx
@@ -7,6 +7,7 @@ import { useEffect } from "react";
 import { gameController } from "@/Controllers/gameController";
 import { useResetPosition } from "@/hooks/useResetPosition";
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import image from '@/assets/images/standinbackground.png';
 
 export const Home = () => {
   const [, setSearchParams] = useSearchParams();
@@ -27,9 +28,16 @@ export const Home = () => {
   return (
     <HelmetProvider>
       <Helmet>
-      <title>Archive of Babel</title>
-      <meta name="description" content="Based on the Library of babel by Louis Borges." />
-      <meta name="keywords" content="Library of babel, online game, infinity" />
+      <title>Archive of Babel - the playable recreation of the short story 'The Library of Babel' by Jorge Louis Borges</title>
+      <meta name="description" content="A browser based 3D game that can be played for free on mobile and desktop. Wonder the endless Library reading the books, searching for esoteric knowledge." />
+        <meta name="keywords" content="Library of babel, online game, infinity" />
+        <meta property="og:title" content="The Archive of Babel"/>
+        <meta property="og:title" content="website"/>
+        <meta property="og:url" content="https://www.archiveofbabel.com"/>
+        <meta property="og:image" content={image} />
+        <meta property="og:description" content='An infinite library to explore in browser, on mobile or desktop. Based on the short story "The Library of Babel" by Jorge Louis Borges.' />
+        <meta property="og:determiner" content="the"/>
+        <meta property="og:locale" content="en_GB"/>
         </Helmet>
     <div className="space-y-[50vh]">
     <Hero />
