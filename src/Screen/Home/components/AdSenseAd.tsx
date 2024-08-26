53a3d9f671c0e7236e3a51f04edc4c86f7c684d9
tommoon <tomasbtmoon@gmail.com>
2024-08-26 14:34:49 +0200

    fix

--- src/Screen/Home/components/AdSenseAd.tsx
+++ src/Screen/Home/components/AdSenseAd.tsx
@@ -2,7 +2,7 @@
 export const AdSenseAd: React.FC<{dataAdSlot: string}> = ({dataAdSlot}) => {
 
     return window.adsbygoogle && window.location.hostname === "archiveofbabel" && (
-        <div className="text-center adsbygoogle mt-2">
+        <div className="text-center mt-2">
             <ins className="adsbygoogle"
                 style={{ display: "block" }}
                 data-ad-client="ca-pub-4658165198708977"
