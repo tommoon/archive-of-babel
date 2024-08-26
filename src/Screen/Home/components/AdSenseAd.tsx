de430bc7c24eaf31ccc4de1c470818ec474f3374
tommoon <tomasbtmoon@gmail.com>
2024-08-26 14:53:25 +0200

    ad fix

--- src/Screen/Home/components/AdSenseAd.tsx
+++ src/Screen/Home/components/AdSenseAd.tsx
@@ -1,16 +1,15 @@
+import { Adsense } from '@ctrl/react-adsense';
 
 export const AdSenseAd: React.FC<{dataAdSlot: string}> = ({dataAdSlot}) => {
 
-    return window.adsbygoogle && window.location.hostname === "archiveofbabel" && (
+    return window.adsbygoogle && (
         <div className="text-center mt-2">
-            <ins className="adsbygoogle"
-                style={{ display: "block" }}
-                data-ad-client="ca-pub-4658165198708977"
-                data-ad-slot={dataAdSlot}
-                data-ad-format="auto"
-                data-full-width-responsive="true"
-            >
-            </ins>
+            <Adsense
+                className='ExampleAdSlot'
+                client="ca-pub-4658165198708977"
+                slot={dataAdSlot}
+                adTest={window.location.hostname === "archiveofbabel" ? '' : 'on'}
+            />
         </div>
     );
 };
